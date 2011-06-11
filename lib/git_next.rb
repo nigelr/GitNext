ef2a253667ba8a8e4cebf4c9daab038270eefc73
Nigel Rausch <nigelr@brisbanerails.com>
2011-06-11 20:08:35 +1000

    refactor config save position

--- lib/git_next.rb
+++ lib/git_next.rb
@@ -4,24 +4,30 @@ class GitNext
 
   CONFIG_FILE = "/.git/gitnext.config"
 
-  def self.run current_path, args=[]
-    git = Git.open current_path
+  def self.config_save_position value
+    File.open(@current_path + CONFIG_FILE, "w") { |f| f.write value }
+  end
 
-    if File.exist?(current_path + CONFIG_FILE)
+  def self.run current_path, args=[]
+    @current_path = current_path
+
+    git = Git.open @current_path
+
+    if File.exist?(@current_path + CONFIG_FILE)
       if args == "top"
 #        git.checkout "master"
 
-        File.open(current_path + CONFIG_FILE, "w") { |f| f.write 0 }
+        config_save_position 0
       else
-        position = File.read(current_path + CONFIG_FILE).to_i
+        position = File.read(@current_path + CONFIG_FILE).to_i
         position -=1
         git.checkout "master~#{position}"
-        File.open(current_path + CONFIG_FILE, "w") { |f| f.write position }
+        config_save_position position
       end
     else
       git_log = git.log.map(&:sha)
       git.checkout git_log.last
-      File.open(current_path + CONFIG_FILE, "w") { |f| f.write git_log.length-1 }
+      config_save_position git_log.length-1
     end
   end
 end
