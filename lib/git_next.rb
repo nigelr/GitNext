879b0ffc83319ecc6cf3d03757563db2299a3517
Nigel Rausch <nigelr@brisbanerails.com>
2011-06-12 10:29:56 +1000

    refactor to common go_to

--- lib/git_next.rb
+++ lib/git_next.rb
@@ -12,35 +12,33 @@ class GitNext
     File.read(@current_path + CONFIG_FILE).to_i
   end
 
-  def self.go_to_bottom(git)
-    git.checkout "master"
-    git_log = git.log.map(&:sha)
-    git.checkout git_log.last
-    config_save_position git_log.length-1
+  def self.get_repo_length
+    @git.checkout "master"
+    @git.log.map(&:sha).length - 1
   end
 
-  def self.go_to(git, position)
-    git.checkout "master#{"~#{position}" if position > 0}"
+  def self.go_to position
+    @git.checkout "master#{"~#{position}" if position > 0}"
     config_save_position position
   end
 
   def self.run current_path, args=[]
     @current_path = current_path
     if File.exists? File.join @current_path, ".git"
-      git = Git.open @current_path
+      @git = Git.open @current_path
 
       if File.exist?(@current_path + CONFIG_FILE)
         if args == "top"
-          go_to git, 0
+          go_to 0
         elsif args == "prev"
-          go_to(git, config_read_position + 1)
+          go_to config_read_position + 1
         elsif args == "bottom"
-          go_to_bottom git
+          go_to get_repo_length
         else
-          go_to(git, config_read_position - 1)
+          go_to config_read_position - 1
         end
       else
-        go_to_bottom(git)
+        go_to get_repo_length
       end
     else
       puts "Not a git directory"
