7f34dc7fa676820015ec9edb7869afce217b5cab
Nigel Rausch <nigelr@brisbanerails.com>
2011-06-12 10:31:03 +1000

    moved methods to rpivate

--- lib/git_next.rb
+++ lib/git_next.rb
@@ -4,24 +4,6 @@ class GitNext
 
   CONFIG_FILE = "/.git/gitnext.config"
 
-  def self.config_save_position value
-    File.open(@current_path + CONFIG_FILE, "w") { |f| f.write value }
-  end
-
-  def self.config_read_position
-    File.read(@current_path + CONFIG_FILE).to_i
-  end
-
-  def self.get_repo_length
-    @git.checkout "master"
-    @git.log.map(&:sha).length - 1
-  end
-
-  def self.go_to position
-    @git.checkout "master#{"~#{position}" if position > 0}"
-    config_save_position position
-  end
-
   def self.run current_path, args=[]
     @current_path = current_path
     if File.exists? File.join @current_path, ".git"
@@ -44,4 +26,25 @@ class GitNext
       puts "Not a git directory"
     end
   end
+
+  private
+
+  def self.config_save_position value
+    File.open(@current_path + CONFIG_FILE, "w") { |f| f.write value }
+  end
+
+  def self.config_read_position
+    File.read(@current_path + CONFIG_FILE).to_i
+  end
+
+  def self.get_repo_length
+    @git.checkout "master"
+    @git.log.map(&:sha).length - 1
+  end
+
+  def self.go_to position
+    @git.checkout "master#{"~#{position}" if position > 0}"
+    config_save_position position
+  end
+  
 end
